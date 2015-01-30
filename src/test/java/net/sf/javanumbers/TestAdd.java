40b9a815e9c985bd74678d537c0e43bbbc56b525
Nathan Lane <nathamberlane@gmail.com>
2015-01-30 11:50:36 -0700

    Adding tests for add function.

---
+++ src/test/java/net/sf/javanumbers/TestAdd.java
@@ -0,0 +1,33 @@
+package net.sf.javanumbers;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestAdd {
+  
+  @Test
+  public void testAddToWhole() {
+    final Rational r1 = new Rational(2, 3);
+    final Rational r2 = new Rational(1, 3);
+    
+    assertTrue(r1.add(r2).equals(1));
+  }
+
+  @Test
+  public void testAddToWhole2() {
+    final Rational r1 = new Rational(7, 8);
+    final Rational r2 = Rational.valueOf(0.125);
+    
+    assertTrue(r1.add(r2).equals(1));
+  }
+  
+  @Test
+  public void testAddRationals() {
+    final Rational r1 = new Rational(1, 16);
+    final Rational r2 = new Rational(2, 16);
+    
+    assertTrue(r1.add(r2).equals(new Rational(3, 16)));
+  }
+
+}
